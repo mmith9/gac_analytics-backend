95e380a5ebf125cff43aa250ee02e520482c110a
Your Name <adam.wronski@gmail.com>
2022-12-06 00:44:02 +0100

    unified and simplified side panels redefined full data request template

--- app/data_classes.py
+++ app/data_classes.py
@@ -7,8 +7,15 @@ class Unit(BaseModel):
     image_url:str
     unit_id:str
     base_id:str
+
+class UnitFreq(Unit):
     count: int | None
     
+class Team(BaseModel):
+    leader: Unit
+    members: list[Unit]
+
+
 class GacSeason(BaseModel):
     type:str
     rounds:list[int]
@@ -16,59 +23,20 @@ class GacSeason(BaseModel):
     startDate:str | None 
     endDate:str | None
     
+
+# return? objects
 class GacBattle(BaseModel):
     attackers: list[int]
-    defenders: list[int] # too heavy to use Team = list[Unit]
-    wins:int
-    losses:int
-    avg_banners:float
-    win_percent:float
-
-class StatLimit(BaseModel):
-    stat_id: int
-    stat_min: int
-    stat_max: int
-    stat_name: str | None
-
-class DatacronCC(BaseModel):
-    ability_3: int | None
-    ability_6: int | None
-    ability_9: int | None
-    stat_limits: list[StatLimit]
-
-
-class Team(BaseModel):
-    leader: Unit
-    members: list[Unit]
-    datacron: DatacronCC | None
-    stat_limits: list[StatLimit] | None
-
-class GacDataRequest(BaseModel):
-    season:GacSeason
-    attackers: Team | None 
-    defenders: Team | None
-    cutoff: int | None
+    defenders: list[int]  # too heavy to use Team = list[Unit]
+    wins: int
+    losses: int
+    avg_banners: float
+    win_percent: float
 
 class GacBattleData(BaseModel):
     battles: list[GacBattle]
-
-    battlesCCin:  list[dict] | None  # recursion is not supported fully
+    battlesCCin:  list[dict] | None 
     battlesCCout: list[dict] | None
-
-# class DCSLRow(BaseModel):
-#     id: int
-#     count: int
-#     long_desc: str
-
-# class DCSeasonList(BaseModel):
-#     season:int
-#     l3: list[DCSLRow]
-#     l6: list[DCSLRow]
-#     l9: list[DCSLRow]
-
-#class PopularLeaders(BaseModel):
-#    attackers: list[int]
-#    defenders: list[int]
 
 class Freq(BaseModel):
     id: int
@@ -86,6 +54,43 @@ class PrecalcObject(BaseModel):
     season: int
     item_type: str
     payload: list[PORow]
+
+
+# frontent classes, also data request model
+
+
+
+class StatLimit(BaseModel):
+    stat_id: int
+    stat_min: int
+    stat_max: int
+
+class DatacronCC(BaseModel):
+    ability_3: int | None
+    ability_6: int | None
+    ability_9: int | None
+    stat_limits: list[StatLimit]
+
+class UnitCC(BaseModel):
+    unit_id: int
+    stat_limits: list[StatLimit] | None
+
+class TeamCC(BaseModel):
+    leader: UnitCC | None
+    members: list[UnitCC]
+    datacron: DatacronCC | None
+
+class WinCC(BaseModel):
+    attempt: list[int] | None
+    defenders_start: list[int]
+    defenders_end: list[int]
+
+class GacDataRequest(BaseModel):
+    season: GacSeason
+    attackers: TeamCC | None
+    defenders: TeamCC | None
+    win_conditions: WinCC | None
+    cutoff: int | None
 
 
 
